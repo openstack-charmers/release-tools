5066f32dc88de2965fa6c1bd01ae9ebda2bd7bcb
James Page <james.page@ubuntu.com>
2016-04-21 15:07:23 +0100

    Add supports series files and tool to run publishing to charm store

--- push-and-publish.sh
+++ push-and-publish.sh
@@ -7,7 +7,7 @@ charm="$1"
 
 charm_url="cs:~openstack-charmers/$series/$charm"
 
-charm_ref="$(charm push . $charm_url | grep url | awk '{ print $2 }')"
+charm_ref="$(charm push $charm $charm_url | grep url | awk '{ print $2 }')"
 if [ -z "$charm_ref" ]; then
     echo "Failed to push charm to charm-store"
     exit 1
